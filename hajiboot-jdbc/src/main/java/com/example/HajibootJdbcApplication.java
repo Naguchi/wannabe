152568407e6505d41696227377bb0c98e8bad4ae
Naguchi <lucrerakuri@gmail.com>
2017-04-30 20:46:24 +0900

    CRUDの追加

--- hajiboot-jdbc/src/main/java/com/example/HajibootJdbcApplication.java
+++ hajiboot-jdbc/src/main/java/com/example/HajibootJdbcApplication.java
@@ -1,40 +1,26 @@
 package com.example;
 
-import java.sql.SQLException;
-
-
 import com.example.domain.Customer;
+import com.example.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 @SpringBootApplication
 public class HajibootJdbcApplication implements CommandLineRunner {
 	@Autowired
-	NamedParameterJdbcTemplate jdbcTemplate;
+	CustomerRepository customerRepository;
 	
 	@Override
 	public void run (String...strings) throws Exception {
-		String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
-		SqlParameterSource param = new MapSqlParameterSource()
-				.addValue("id", 1);
+		// データ追加
+		Customer created = customerRepository.save(new Customer(null, "Hidetoshi", "Dekisugi"));
+		System.out.println(created + " is created!");
 		
-		Customer result = jdbcTemplate.queryForObject(sql, param, new RowMapper<Customer>() {
-			@Override
-			public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
-				return new Customer(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name"));
-			}
-		});
-		
-		System.out.println("result = " + result);
+		// データ表示
+		customerRepository.findAll()
+			.forEach(System.out::println); // (1)
 	}
 
 	public static void main(String[] args) {
